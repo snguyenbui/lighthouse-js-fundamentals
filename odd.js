47068e74be0ce49422ba0331ac146ee9e0723154
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-01-27 23:03:51 +0000

    Determining if a number is odd or not

---
+++ odd.js
@@ -0,0 +1,8 @@
+//Odd checker - declaring a function which takes a number parameter and determines if it's odd
+let isOdd = function (num) {
+  return num % 2 === 1;
+}
+
+//Testing isOdd
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
