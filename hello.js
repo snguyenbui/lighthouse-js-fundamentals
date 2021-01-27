5e0f9e48e31949ac86ebd9d3c1f2eb0c95f4bc6d
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-01-27 22:25:04 +0000

    Declaring functions to print Hello

---
+++ hello.js
@@ -0,0 +1,17 @@
+//Example 1 - declaring a function
+/*
+const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();
+*/
+
+//Example 2 - declaring a function with parameters
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
