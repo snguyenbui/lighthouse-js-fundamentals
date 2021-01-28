7b83674bc7aaa55e6d194025715891a5fe203a13
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-01-28 21:23:33 +0000

    Iterating through arrays with while loops

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,19 @@
 //Example 1 - iterating through an array with a for loop
-const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+/*
 console.log("Kitchen stuff to pack:");
 //For loop
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+//Example 2 - iterating through an array with a while loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
