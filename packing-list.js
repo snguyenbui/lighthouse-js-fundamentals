7eba12cc1eb517053c90f825342b87c009f3e92d
Sunny Nguyen-Bui <snguyenbui@gmail.com>
2021-01-28 21:21:06 +0000

    Iterating through arrays with for loops

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+//Example 1 - iterating through an array with a for loop
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+//For loop
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
